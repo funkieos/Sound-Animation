34a1157b38dc2c9d8d5bde6ca62607a04168f23d
unknown <jrockz990@gmail.com>
2021-10-03 21:33:13 -0400

    Animation commit

--- Scripts/NewBehaviourScript.cs
+++ Scripts/NewBehaviourScript.cs
@@ -11,37 +11,46 @@ public class NewBehaviourScript : MonoBehaviour
 
     public AudioSource musicSource;
 
-
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("escape"))
+        {
+          Application.Quit();
+        }
+        
         if (Input.GetKeyDown(KeyCode.W))
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
         }  
     }
 }
